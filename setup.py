9b4005677472cb8ca226a6d9452746d47df64c27
Rafael Soares <rafaelsantos88@gmail.com>
2014-03-06 12:52:58 -0300

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+
+from distutils.core import setup
+setup(
+    name='py2docx',
+    version='0.1a1',
+    description='Write .docx documents using Python Code',
+    long_description="",
+    keywords=['docx', 'word', 'microsoft word'],
+    author='Rafael Soares',
+    author_email='rafaeltravel88@gmail.com',
+    url='http://github.com/rafaels88/py2docx',
+    license='MIT',
+    packages=['py2docx', 'py2docx.elements', 'py2docx.tmp'],
+    include_package_data=True,
+    install_requires=['PIL>=1.0']
+)
